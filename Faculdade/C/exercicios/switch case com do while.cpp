7328cb7251bcd2a6bd6ecc7d7a5502dfce2c3207
Fzin <fabricio.silvaramos2001@gmail.com>
2023-04-24 00:08:41 -0300

    switch case + do while

---
+++ Faculdade/C/exercicios/switch case com do while.cpp
@@ -0,0 +1,77 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+
+int main(){
+	
+	setlocale(LC_ALL, "portuguese");
+	
+	int opcao;
+	float n1,n2,resultado;
+	
+	do{
+	
+	
+	printf("---- MENU ----\n");
+	printf("|1 - Soma       \n");
+	printf("|2 - subtrair   \n");
+	printf("|3 - multiplicar\n");
+	printf("|4 - dividir  \n\n");
+	printf("Selecione uma opcao (1-4): ");
+	
+	scanf("%d",&opcao);
+	
+	switch(opcao){
+		
+	    case 1: 
+	    	
+	    	
+	    	printf("Digite o primeiro numero: ");
+			scanf("%f",&n1);
+			printf("Digite o segundo numero: ");
+			scanf("%f",&n2);
+			resultado = n1+n2;
+			printf("O resultado foi: %.1f",resultado);
+			break;
+			
+		case 2:	
+		
+		printf("Digite o primeiro numero: ");
+			scanf("%f",&n1);
+			printf("Digite o segundo numero: ");
+			scanf("%f",&n2);
+			resultado = n1-n2;
+			printf("O resultado foi: %.1f",resultado);
+			break;
+			
+		case 3:	
+		
+		printf("Digite o primeiro numero: ");
+			scanf("%f",&n1);
+			printf("Digite o segundo numero: ");
+			scanf("%f",&n2);
+			resultado = n1*n2;
+			printf("O resultado foi: %.1f",resultado);
+			break;
+			
+		case 4:	
+		
+		printf("Digite o primeiro numero: ");
+			scanf("%f",&n1);
+			printf("Digite o segundo numero: ");
+			scanf("%f",&n2);
+			resultado = n1/n2;
+			printf("O resultado foi: %.1f",resultado);
+			break;
+		
+		default:
+			printf("Opção inválida. Tente novamente.\n\n");
+      
+	 }
+	
+	} while (opcao > 4);
+		
+}
+	
+	
+
