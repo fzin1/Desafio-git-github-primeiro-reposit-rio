7b723e204e7a9a4989404902eecec1e4f0c4dd4b
Fzin <131416632+fzin1@users.noreply.github.com>
2023-04-24 19:45:29 -0300

    deixando o código mais bonito

--- Faculdade/C/exercicios/switch case com do while.cpp
+++ Faculdade/C/exercicios/switch case com do while.cpp
@@ -3,29 +3,27 @@
 #include<locale.h>
 
 int main(){
-	
+
 	setlocale(LC_ALL, "portuguese");
-	
+
 	int opcao;
 	float n1,n2,resultado;
-	
+
+	printf("----- MENU -----\n");
+	printf("|1 - Soma       |\n");
+	printf("|2 - subtrair   |\n");
+	printf("|3 - multiplicar|\n");
+	printf("|4 - dividir    |\n\n");
 	do{
-	
-	
-	printf("---- MENU ----\n");
-	printf("|1 - Soma       \n");
-	printf("|2 - subtrair   \n");
-	printf("|3 - multiplicar\n");
-	printf("|4 - dividir  \n\n");
 	printf("Selecione uma opcao (1-4): ");
-	
+
 	scanf("%d",&opcao);
-	
+
 	switch(opcao){
-		
-	    case 1: 
-	    	
-	    	
+
+	    case 1:
+
+
 	    	printf("Digite o primeiro numero: ");
 			scanf("%f",&n1);
 			printf("Digite o segundo numero: ");
@@ -33,9 +31,9 @@ int main(){
 			resultado = n1+n2;
 			printf("O resultado foi: %.1f",resultado);
 			break;
-			
-		case 2:	
-		
+
+		case 2:
+
 		printf("Digite o primeiro numero: ");
 			scanf("%f",&n1);
 			printf("Digite o segundo numero: ");
@@ -43,9 +41,9 @@ int main(){
 			resultado = n1-n2;
 			printf("O resultado foi: %.1f",resultado);
 			break;
-			
-		case 3:	
-		
+
+		case 3:
+
 		printf("Digite o primeiro numero: ");
 			scanf("%f",&n1);
 			printf("Digite o segundo numero: ");
@@ -53,9 +51,9 @@ int main(){
 			resultado = n1*n2;
 			printf("O resultado foi: %.1f",resultado);
 			break;
-			
-		case 4:	
-		
+
+		case 4:
+
 		printf("Digite o primeiro numero: ");
 			scanf("%f",&n1);
 			printf("Digite o segundo numero: ");
@@ -63,15 +61,12 @@ int main(){
 			resultado = n1/n2;
 			printf("O resultado foi: %.1f",resultado);
 			break;
-		
-		default:
-			printf("Opção inválida. Tente novamente.\n\n");
-      
-	 }
-	
-	} while (opcao > 4);
-		
-}
-	
-	
 
+		default:
+			printf("\nOpÃ§Ã£o invÃ¡lida. Tente novamente.\n\n");
+
+	 }
+
+	} while (opcao > 4);
+
+}
