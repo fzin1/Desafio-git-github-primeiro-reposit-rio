9010d402316b148cdbabb9d959d2ebadbf2e0842
Fzin <131416632+fzin1@users.noreply.github.com>
2023-04-24 20:51:51 -0300

    if else idade

--- Faculdade/C/exercicios/if elseif idade.c
+++ Faculdade/C/exercicios/if elseif idade.c
@@ -3,23 +3,87 @@
 
 int main(){
 
-int idade;
+int mes;
 
-printf("Qual a sua idade?: ");
-scanf("%d",&idade);
-if (idade<0 || idade > 130)
-    printf("voce nao existe");
+do {
 
-else if (idade<18)
-    printf("Voce e jovem");
+printf("Digite um valor entre 1-12: ");
+scanf("%d",&mes);
+
+switch(mes){
+
+case 1:
+    printf("janeiro");
+    break;
+
+case 2:
+    printf("fevereiro");
+    break;
+
+case 3:
+    printf("março");
+    break;
+
+case 4:
+    printf("abril");
+    break;
+
+case 5:
+    printf("maio");
+    break;
+
+case 6:
+    printf("junho");
+    break;
+
+case 7:
+    printf("julho");
+    break;
+
+case 8:
+    printf("agosto");
+    break;
+
+case 9:
+    printf("setembro");
+    break;
+
+case 10:
+    printf("outubro");
+    break;
+
+case 11:
+    printf("novembro");
+    break;
+
+case 12:
+    printf("dezembro");
+    break;
+
+default: printf("erro\n");
 
 
-else if(idade<=60)
-    printf("Voce e adulto");
+}
+}while (mes > 12 || mes < 0);
 
 
-else
-    printf("Voce e idoso");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
 
