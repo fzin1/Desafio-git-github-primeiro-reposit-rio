5b327e693db38a3736f6c669cc7423c2e1fd8d02
Fzin <fabricio.silvaramos2001@gmail.com>
2023-05-01 02:50:57 -0300

    numero divisivel com funcao

---
+++ javascript/04 - funções/numerodivisivelcomfuncao.js
@@ -0,0 +1,13 @@
+
+//como ler numero de 1-100 divisivel por 5
+
+function divisivel(numero,divisor){
+    if (numero % divisor === 0){
+        console.log(numero + ' é divisivel por ' + divisor)
+    }else if(numero % divisor !== 0){
+        console.log(numero + ' não é divisivel por ' + divisor)
+    }
+    }
+    
+    divisivel(11,5)
+    
